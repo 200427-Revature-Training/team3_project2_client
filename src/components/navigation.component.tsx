5655327ea1c04bc1e623104dbb23255e3af6bbb6
Salem Aouchiche <salem.aouchiche@gmail.com>
2020-06-18 23:41:36 -0700

    worked on  signin and signup.

--- src/components/navigation.component.tsx
+++ src/components/navigation.component.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import { colors } from '@material-ui/core';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }),
+);
 
+const NavigationComponent:React.FC=()=>{
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            GOFundMe 
+            <Button color="primary"> <Link to="/"> Home </Link> </Button>
+            <Button color="primary" > <Link to="/Help"> Help </Link> </Button>
+          </Typography>
+          <Button color="primary" > <Link to="/Login"> Sign-in |</Link> </Button>
+          <Button color="primary"> <Link to="/SignUp"> Sing-up </Link> </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default NavigationComponent; 
+
+/*
 const NavigationComponent:React.FC=()=>{
     
     return (
@@ -15,4 +59,6 @@ const NavigationComponent:React.FC=()=>{
        
     )
 }
-export default NavigationComponent; 
+
+
+*/
---
+++ src/components/page/home/home.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const HomeComponent:React.FC=()=> {
+ 
+
+  return (
+    <div> home page</div>
+  )
+}
+export default HomeComponent;

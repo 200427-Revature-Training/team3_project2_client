b41d837072e1e8acdd54eac39b6daacd90355e9d
Salem Aouchiche <salem.aouchiche@gmail.com>
2020-06-18 18:01:52 -0700

    fixed nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
 import MainComponent from './components/main.component';
-import NavigationComponent from './components/navigation.component';
 
 function App() {
   return (
     <div className="App">
       fund all app
       <MainComponent></MainComponent>
-      <NavigationComponent></NavigationComponent>
     </div>
+    
   );
 }
 

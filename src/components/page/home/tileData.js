0c2cf6d0929c7994c5468c8bc12d5644384b6000
Nmposer4927 <42839367+Nmposer4927@users.noreply.github.com>
2020-06-24 16:14:11 -0400

    homecomponent started

---
+++ src/components/page/home/tileData.js
@@ -0,0 +1,69 @@
+const tileData = [
+    {
+      img: '/static/images/grid-list/breakfast.jpg',
+      title: 'Breakfast',
+      author: 'user1',
+      cols: 2,
+      featured: true,
+    },
+    {
+      img: '/static/images/grid-list/burgers.jpg',
+      title: 'Tasty burger',
+      author: 'user2',
+    },
+    {
+      img: '/static/images/grid-list/camera.jpg',
+      title: 'Camera',
+      author: 'user3',
+    },
+    {
+      img: '/static/images/grid-list/morning.jpg',
+      title: 'Morning',
+      author: 'user4',
+      featured: true,
+    },
+    {
+      img: '/static/images/grid-list/hats.jpg',
+      title: 'Hats',
+      author: 'user5',
+    },
+    {
+      img: '/static/images/grid-list/honey.jpg',
+      title: 'Honey',
+      author: 'user6',
+    },
+    {
+      img: '/static/images/grid-list/vegetables.jpg',
+      title: 'Vegetables',
+      author: 'user6',
+      cols: 2,
+    },
+    {
+      img: '/static/images/grid-list/plant.jpg',
+      title: 'Water plant',
+      author: 'user7',
+    },
+    {
+      img: '/static/images/grid-list/mushroom.jpg',
+      title: 'Mushrooms',
+      author: 'user8',
+    },
+    {
+      img: '/static/images/grid-list/olive.jpg',
+      title: 'Olive oil',
+      author: 'user9',
+    },
+    {
+      img: '/static/images/grid-list/star.jpg',
+      title: 'Sea star',
+      cols: 2,
+      author: 'user10',
+    },
+    {
+      img: '/static/images/grid-list/bike.jpg',
+      title: 'Bike',
+      author: 'user11',
+    },
+  ];
+  
+  export default tileData;

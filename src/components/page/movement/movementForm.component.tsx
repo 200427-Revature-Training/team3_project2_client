348274f23b2f2e4279069b4f46b13e029ac6c944
Anti Evo <jpujals@bellsouth.net>
2020-07-05 05:31:49 -0400

    Added create new movement form to navbar with logged in bool

--- src/components/page/movement/movementForm.component.tsx
+++ src/components/page/movement/movementForm.component.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Movement } from '../../models/Movement';
+import * as userRemote from '../../remotes/user.remote';
+
 
 /**interface MovementFormComponentProps {
     setView: (str: 'MOVEMENT') => void;
@@ -7,9 +9,9 @@ import { Movement } from '../../models/Movement';
 }
 */
 
-const MovementFormComponent:React.FC = () => {
+const MovementFormComponent: React.FC = () => {
 
-  /**   const [moveName, setMoveName] = useState('');
+    const [moveName, setMoveName] = useState('');
     const [moveGoal, setMoveGoal] = useState('');
     const [moveStart, setMoveStart] = useState('');
     const [moveDescription, setMoveDescription] = useState('');
@@ -17,91 +19,122 @@ const MovementFormComponent:React.FC = () => {
     const [moveImage, setMoveImage] = useState('');
 
     const saveMovement = () => {
-        const movement = {
-            moveName, moveGoal, moveStart, moveDescription, moveType, moveImage
+        /*
+        let m: Movement = {
+           id: 0,
+           name: moveName,
+            goal: moveGoal,
+            current: 0,
+            start: moveStart, 
+             description: moveDescription, 
+             type: { id: moveType, movementType: "2" },            
+              moveImage
         };
         //props.addMovement(movement);
-        props.setView('MOVEMENT');
-
-    }
+        //props.setView('MOVEMENT');
 */
+    }
+
+/*
+    var m: Movement[] = [{
+        id: 0,
+        goal: 1,
+        current: 0,
+        start: "0",
+        description: "test",
+        author: {
+          id: 0,
+          email: 'string',
+          password: 'string',
+          firstName: 'string',
+          lastName: 'string',
+          roleId: { id: 1, userRole: "2" }
+        },
+        approver: "User | string | number; ",
+        status: { id: 1, movementStatus: "2" },
+        type: { id: 1, movementType: "2" },
+        image: './images/flood.jpg',
+        name: 'name'
     
-  
-        return (
-            <section id="movementForm-container">
-                <header>
-                    <h2>Register New Movement</h2>
-                </header>
-    
-                <form>
-                    <div>
-                        <label><div>Movement Name:</div>
-                        <input 
-                            //value={moveName} 
-                            //onChange={(e) => setMoveName(e.target.value)} 
-                            type="text" />
-                        </label>
-                   </div>
-    
-                    <div>
-                        <label><div>Movement Goal</div>
+      }];
+*/
+
+    return (
+        <section id="movementForm-container">
+            <header>
+                <h2>Register New Movement</h2>
+            </header>
+
+            <form>
+                <div>
+                    <label><div>Movement Name:</div>
                         <input
-                        // value={moveGoal} 
-                          //  onChange={(e) => setMoveGoal(e.target.value)}
+                            value={moveName}
+                            onChange={(e) => setMoveName(e.target.value)}
                             type="text" />
-                        </label>
-                    </div>
-    
-                    <div>
-                        <label><div>Movement Start:</div>
-                        <input 
-                            //value={moveStart} 
-                            //onChange={(e) => setMoveStart(e.target.value)}
-                            type="date" />
-                        </label>
-                    </div>
-    
-                    <div>
-                        <label><div>Movement Description:</div>
-                        <textarea 
-                            //value={moveDescription} 
-                            //onChange={(e) => setMoveDescription(e.target.value)}
-                         />
-                        </label>
-                    </div>
+                    </label>
+                </div>
 
-                    <div>
-                        <label><div>Movement Type:</div>
-                        <input 
-                        //value={moveType} 
-                         //   onChange={(e) => setMoveType(e.target.value)}
+                <div>
+                    <label><div>Movement Goal</div>
+                        <input
+                            value={moveGoal}
+                            onChange={(e) => setMoveGoal(e.target.value)}
                             type="number" />
-                        </label>
-                    </div>
+                    </label>
+                </div>
 
-                    <div>
-                        <label><div>Movement Image:</div>
-                        <input 
-                         //value={moveImage} 
-                           // onChange={(e) => setMoveImage(e.target.value)}
+                <div>
+                    <label><div>Movement Start:</div>
+                        <input
+                            value={moveStart}
+                            onChange={(e) => setMoveStart(e.target.value)}
+                            type="date" />
+                    </label>
+                </div>
+
+                <div>
+                    <label><div>Movement Description:</div>
+                        <textarea
+                            value={moveDescription}
+                            onChange={(e) => setMoveDescription(e.target.value)}
+                        />
+                    </label>
+                </div>
+
+                <div>
+                    <label><div>Movement Type:</div>
+                        <input
+                            value={moveType}
+                            onChange={(e) => setMoveType(e.target.value)}
+                            type="number" />
+                    </label>
+                </div>
+
+                <div>
+                    <label><div>Movement Image:</div>
+                        <input
+                            value={moveImage}
+                            onChange={(e) => setMoveImage(e.target.value)}
                             type="file" accept="image/*" />
-                        </label>
-                    </div>
-    
-        
-                    <button>Save Movement</button>
-            
-    
-                </form>
-    
-            </section>
-            
-            
+                    </label>
+                </div>
+
+
+                <button onClick={() => saveMovement()}>Create Movement</button>
+
+
+
+            </form>
+
+        </section>
+
+
 
     );
 }
 
-export default MovementFormComponent; 
+export default MovementFormComponent;
 
 /**
  *    <div>
